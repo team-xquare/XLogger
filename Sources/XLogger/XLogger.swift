2ff5ca6ae81c1d3889c388e4e37ff1aa58f3ba0b
Martin <67373938+kimxwan0319@users.noreply.github.com>
2023-03-29 23:17:43 +0900

    ♻️ :: 로그 전후로 줄바꿈 추가

--- Sources/XLogger/XLogger.swift
+++ Sources/XLogger/XLogger.swift
@@ -3,29 +3,39 @@ import Foundation
 public struct XLog {
 
     public static func network(_ any: Any, filename: String = #file, funcName: String = #function) {
+        print("\n\n")
         print("[🛰️Network][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n\n")
     }
 
     public static func info(_ any: Any, filename: String = #file, funcName: String = #function) {
+        print("\n\n")
         print("[ℹ️Info][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n\n")
     }
     
     public static func error(_ any: Any, filename: String = #file, funcName: String = #function) {
+        print("\n\n")
         print("[🚨Error][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n\n")
     }
 
     // MARK: - Personal Log :: Local only
     // 6기 수완's Log
     public static func martin(_ any: Any, filename: String = #file, funcName: String = #function) {
         #if DEBUG
+        print("\n")
         print("[🦍Martin][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n")
         #endif
     }
     
     // 7기 기영's Log
     public static func hero(_ any: Any, filename: String = #file, funcName: String = #function) {
         #if DEBUG
+        print("\n")
         print("[🐈‍⬛Hero][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n")
         #endif
     }
     
@@ -33,7 +43,9 @@ public struct XLog {
     // 8기 주영's Log
     public static func juy(_ any: Any, filename: String = #file, funcName: String = #function) {
         #if DEBUG
+        print("\n")
         print("[🐩Juy][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n")
         #endif
     }
     
@@ -41,7 +53,9 @@ public struct XLog {
     // 8기 병진's Log
     public static func cho(_ any: Any, filename: String = #file, funcName: String = #function) {
         #if DEBUG
+        print("\n")
         print("[🥑Cho][\(sourceFileName(filePath: filename))] \(funcName) -> \(any)")
+        print("\n")
         #endif
     }
 
